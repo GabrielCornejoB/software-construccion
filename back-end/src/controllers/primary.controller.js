31139c81a6033195c3c7228002b633f4794e7c21
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-03-12 11:43:42 -0500

    Refactoring endpoint: update primary

--- back-end/src/controllers/primary.controller.js
+++ back-end/src/controllers/primary.controller.js
@@ -34,3 +34,60 @@ exports.addPrimary = async (req, res) => {
         else res.status(500).send("" + error);
     }
 }
+
+exports.getPrimaries = async (req, res) => {
+    try {
+        const primaries = await Primary.aggregate([{
+            $lookup: 
+            {
+                from: "suppliers",
+                localField: "defaultSupplier",
+                foreignField: "id",
+                as: "supplierName"
+            }
+        }]).exec();
+        const output = [];
+        for (let obj of primaries) {
+            let supplier = "-";
+            if (obj.supplierName[0]) {
+                supplier = obj.supplierName[0].supplier;
+            }
+            output.push(
+                {
+                    id: obj.id,
+                    primary: obj.primary,
+                    group: obj.group,
+                    clasification: obj.clasification,
+                    unit: obj.unit,
+                    defaultPrice: obj.defaultPrice,
+                    defaultSupplier: supplier
+                }
+            )
+        }
+        res.json(output);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.updatePrimary = async (req, res) => {
+    try {
+        const { primary, group, clasification, unit } = req.body;
+        const primaryIdExists = await Primary.findOne({id: req.params.id});
+        const primaryExists = await Primary.findOne({primary: primary});
+        if (!primaryIdExists) return res.status(404).json({msg: "Primary doesn't exist"});    
+        if (primaryExists) return res.status(400).json({msg: "Primary already exists"});
+
+        primaryIdExists.primary = primary;
+        primaryIdExists.group = group;
+        primaryIdExists.clasification = clasification;
+        primaryIdExists.unit = unit;
+        await primaryIdExists.save();
+        res.json(primaryIdExists);
+    } catch (error) {
+        if (error.errors?.unit) return res.status(400).send("Invalid or missing field 'unit'");
+        else if (error.errors?.clasification) return res.status(400).send("Invalid or missing field 'clasification'");
+        else if (error.errors?.group) return res.status(400).send("Invalid or missing field 'group'");
+        else return res.status(500).send("" + error);
+    }
+}
--- back-end/src/routes/primary.route.js
+++ back-end/src/routes/primary.route.js
@@ -1,43 +1,12 @@
 const { Router } = require("express");
 const router = Router();
-const { Unit, Group, Clasification, getKeys } = require('../models/enums');
-const counterModel = require('../models/counter.model');
-const primaryModel = require('../models/primary.model');
-const supplierModel = require('../models/supplier.model');
-const { addPrimary } = require("../controllers/primary.controller");
+const primaryController = require("../controllers/primary.controller");
 
-router.post('/add-primary', addPrimary);
+router.post('/add-primary', primaryController.addPrimary);
 
-// router.post('/add-primary', async (req, res) => {
-//     const { primary, group, clasification, unit } = req.body;
+router.get('/get-primaries', primaryController.getPrimaries);
 
-//     if (!primary || !primary?.trim()) return res.status(400).send("Missing fields");
-//     const primaryExists = await primaryModel.findOne({ primary: primary });
-//     if (primaryExists) return res.status(400).send("Primary '" + primary + "' already exists");
-//     if (!getKeys(Unit).includes(unit)) return res.status(400).send("Unit '" + unit + "' is not valid");
-//     if (!getKeys(Group).includes(group)) return res.status(400).send("Group '" + group + "' is not valid");
-//     if (!getKeys(Clasification).includes(clasification)) return res.status(400).send("Clasification '" + clasification + "' is not valid");
-//     const primaryCounterExists = await counterModel.findOneAndUpdate({ collectionName: "primaries" }, { $inc: { "counter": 1 }});
-//     if (!primaryCounterExists) {
-//         const newCounter = new counterModel({collectionName: "primaries", counter: 1});
-//         await newCounter.save();
-//         counter = 1;
-//     }
-//     else counter = primaryCounterExists.counter + 1;
-
-//     const newPrimary = new primaryModel({ 
-//         id: counter,
-//         primary, 
-//         group, 
-//         clasification, 
-//         unit, 
-//         defaultPrice: 0,
-//         defaultSupplier: 0
-//     });
-//     await newPrimary.save();
-//     let answerString = "'" + primary + "' added succesfully";
-//     res.json({ msg: answerString });
-// });
+router.put('/update-primary/:id', primaryController.updatePrimary);
 
 // router.post('/add-supplier-to-primary', async (req, res) => {
 //     const {primaryId, supplierId, listPrice, iva, discount} = req.body;
@@ -64,25 +33,6 @@ router.post('/add-primary', addPrimary);
 //     primaryIdExists.suppliers.push(values);
 //     await primaryIdExists.save();
 //     return res.status(200).send("Supplier added to " + primaryIdExists.primary);
-// });
-
-// router.put('/update-primary/:id', async (req, res) => {
-//     const { primary, group, clasification, unit} = req.body;
-//     if (!primary?.trim()) return res.status(400).send("Missing fields");
-//     const primaryIdExists = await primaryModel.findOne({id: req.params.id});
-//     if (!primaryIdExists) return res.status(400).send("Primary doesn't exist");
-//     const primaryExists = await primaryModel.findOne({ primary: primary });
-//     if (primaryExists) return res.status(400).send("Primary '" + primary + "' already exists");
-//     if (!getKeys(Unit).includes(unit)) return res.status(400).send("Unit '" + unit + "' is not valid");
-//     if (!getKeys(Group).includes(group)) return res.status(400).send("Group '" + group + "' is not valid");
-//     if (!getKeys(Clasification).includes(clasification)) return res.status(400).send("Clasification '" + clasification + "' is not valid");
-
-//     primaryIdExists.primary = primary;
-//     primaryIdExists.group = group;
-//     primaryIdExists.clasification = clasification;
-//     primaryIdExists.unit = unit;
-//     await primaryIdExists.save();
-//     return res.status(200).send("Primary updated succesfully");
 // });
 
 // router.patch('/update-supplier-of-primary', async (req, res) => {
