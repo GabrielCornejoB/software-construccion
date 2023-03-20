b6e6f6f8b868ad5dc7ed8f63b585eac9a4005606
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-03-20 16:49:25 -0500

    Suppliers se muestran en UI

--- front-end/src/app/components/suppliers/suppliers-main/suppliers-main.component.ts
+++ front-end/src/app/components/suppliers/suppliers-main/suppliers-main.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SuppliersService } from 'src/app/services/suppliers.service';
+import { Supplier } from 'src/app/types/Supplier';
 
 @Component({
   selector: 'app-suppliers-main',
   templateUrl: './suppliers-main.component.html',
   styleUrls: ['./suppliers-main.component.sass']
 })
-export class SuppliersMainComponent {
+export class SuppliersMainComponent implements OnInit {
+  suppliers: Supplier[] = [];
 
+  constructor(private suppliersService: SuppliersService){}
+
+  ngOnInit(): void {
+    this.getSuppliers();
+  }
+  getSuppliers(): void {
+    this.suppliersService.getSuppliers().subscribe(data => {
+      this.suppliers = data;
+    }, error => {
+      console.log(error);
+    })
+  }
 }
